61dabb397f184749fe8697869a5c27a2bff53e45
Tom <Tomtom22@gmail.com>
2021-05-15 10:56:50 +0200

    Page login CSS

---
+++ Frontend/src/components/Login.js
@@ -0,0 +1,22 @@
+const Login = () => {
+    return (
+        <div className="main">
+            <div className="welcome">Bienvenue</div>
+            <div className="filet"></div>
+            <div className="connect">Connectez-vous</div>
+            <form className="login-form">
+                <label htmlFor="email">email <span className="red">* </span></label>
+                <input type="email" id="email" />
+                <div className="error-email"></div>
+                <label htmlFor="password">mot de passe <span className="red">* </span></label>
+                <input type="password" id="password" />
+                <div className="error-password"> &lt; message d'erreur &gt;</div>
+                <button className="bt-connexion">Connexion</button>
+                <div className="signin-link">Vous n'avez pas de compte ? <a href="https://developer.mozilla.org/fr/docs/Web/CSS/display"> inscrivez-vous</a></div>
+                <div className="required-field"><span className="red">* </span>Champs obligatoires</div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
